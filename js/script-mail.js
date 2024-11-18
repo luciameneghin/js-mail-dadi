02a902b8261b4cc09e6017068709d1fb6e5369f9
luciameneghin <lucia.meneghin00@gmail.com>
2024-11-18 17:28:01 +0100

    fix : 1 exercise

--- js/script-mail.js
+++ js/script-mail.js
@@ -3,15 +3,19 @@
 const validMail = ['nathan.smith@example.com', 'ed.jones@example.com', 'fabio.rossi@example.com', 'phil.brown@example.com', 'carlo.bianchi@example.com'];
 const addMail = prompt('Inserisci qui la tua mail');
 let checkedMail = false;
+let message = 'la tua mail NON è valida';
 
 for (let i = 0; i < validMail.length; i++) {
   if (validMail[i] === addMail) {
     checkedMail = true;
-    alert('La tua mail è valida');
-  } else {
-    alert('La tua mail NON è valida');
+    message = 'la tua mail è valida';
+    // alert('La tua mail è valida');
   }
+  // else {
+  //   message = 'la tua mail NON è valida';
+  // }
 }
+console.log(message);
 
 // for (let i = 0; i < validMail.length; i++) {
 //   if (addMail.includes(validMail)) {
